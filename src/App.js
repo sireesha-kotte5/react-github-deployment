85a320d14984f5ee285858929b062c64d6e6d692
Bala 3nadh <bala.jangam@darkmatter.ae>
2023-05-14 21:40:12 +0530

    version2-change

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           React Github Deployment Application
         </p>
+        <p>
+          React Github Deployment Application changes to verify  version 2
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
